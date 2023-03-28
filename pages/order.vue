cca0eec7acbf72f5a8c032d875cb85b4a5226e06
Ian <passionary003@gmail.com>
2023-03-28 21:05:36 +0300

    fix(client): add Home btn for product page and for order - YIMA-100

--- pages/order.vue
+++ pages/order.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="flex flex-col gap-6 sm:gap-10 xl:gap-16">
+    <div class="container">
+      <TheLink to="/" class="btn btn-primary mt-2 flex w-fit items-center gap-2"> На головну </TheLink>
+    </div>
     <TheSteps />
     <NuxtPage />
   </div>
